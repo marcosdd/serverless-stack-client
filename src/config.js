2b39f3d6ed5836fe0d58a18211552f305235d1cb
Marcos Diniz Duarte <marcos.duarte@smarkio.com.br>
2020-04-26 14:19:02 -0300

    Updated configs

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@ const dev = {
   STRIPE_KEY: "pk_test_6SzfiGAyZb9fSBPmMAPOsGSh00MBKKYX50",
   s3: {
       REGION: "us-east-1",
-      BUCKET: "mduarte-notes-app-uploads"
+      BUCKET: "notes-app-2-api-dev-attachmentsbucket-wew0l88wbiy3"
   },
   apiGateway: {
       REGION: "us-east-1",
-      URL: "https://r15efw55be.execute-api.us-east-1.amazonaws.com/prod"
+      URL: "https://api.marcosdd.com/dev"
   },
   cognito: {
       REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_heX3yg4Oc",
-      APP_CLIENT_ID: "bu60l1spfl8fvatpemh64c2mb",
-      IDENTITY_POOL_ID: "us-east-1:18a7093c-9566-43d3-bd02-a7853868f4fd"
+      USER_POOL_ID: "us-east-1_BZ5lp8a3F",
+      APP_CLIENT_ID: "7ad6b7kvj6a63sfa9ho304fe8q",
+      IDENTITY_POOL_ID: "us-east-1:b38ae192-0678-4bb1-8e83-2d82f9fdf892"
   }
 };
 
@@ -20,17 +20,17 @@ const prod = {
   STRIPE_KEY: "pk_test_6SzfiGAyZb9fSBPmMAPOsGSh00MBKKYX50",
   s3: {
       REGION: "us-east-1",
-      BUCKET: "mduarte-notes-app-uploads"
+      BUCKET: "notes-app-2-api-prod-attachmentsbucket-1m68rtfy5yvv1"
   },
   apiGateway: {
       REGION: "us-east-1",
-      URL: "https://r15efw55be.execute-api.us-east-1.amazonaws.com/prod"
+      URL: "https://api.marcosdd.com/prod"
   },
   cognito: {
       REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_heX3yg4Oc",
-      APP_CLIENT_ID: "bu60l1spfl8fvatpemh64c2mb",
-      IDENTITY_POOL_ID: "us-east-1:18a7093c-9566-43d3-bd02-a7853868f4fd"
+      USER_POOL_ID: "us-east-1_yiVxSrbFF",
+      APP_CLIENT_ID: "3f3q4j12b6bl90uqdrei1ou9kf",
+      IDENTITY_POOL_ID: "us-east-1:36e36fea-540a-4f9a-ba0c-1eddab16f67e"
   }
 };
 
